0970ffad7def0eeb8dc37bd3631d113d931b571b
AKMAL <akmalhamdanak@gmail.com>
2023-10-16 20:38:52 +0800

    add testing.js file

--- testing.js
+++ testing.js
@@ -1,1 +1,11 @@
-TextDecoderStream;
+// Function to calculate the sum of two numbers
+function calculateSum(a, b) {
+  return a + b;
+}
+
+// Test the function
+const num1 = 5;
+const num2 = 10;
+const sum = calculateSum(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
